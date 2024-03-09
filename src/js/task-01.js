192147c66c94dc71a5b8a7b04c2acac49ef06a64
Ayten <139014081+Bonefletcher24@users.noreply.github.com>
2024-03-09 19:25:51 +0400

    upd

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,21 +1,16 @@
-// Находим элемент ul#categories
 const categoriesList = document.getElementById('categories');
 
-// Получаем все элементы li.item внутри ul#categories
 const categoryItems = categoriesList.querySelectorAll('.item');
 
-// Выводим количество категорий
 console.log(`Number of categories: ${categoryItems.length}`);
 
-// Для каждого элемента li.item
 categoryItems.forEach((categoryItem) => {
-  // Находим заголовок категории (тег h2)
   const categoryTitle = categoryItem.querySelector('h2').textContent;
 
-  // Находим все элементы li внутри текущей категории
   const categoryElements = categoryItem.querySelectorAll('ul li');
 
-  // Выводим информацию о категории
   console.log(`\nCategory: ${categoryTitle}`);
   console.log(`Elements: ${categoryElements.length}`);
 });
+
+
